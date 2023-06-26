e55273ba3a41d6d3441f52113d35d3733d886ca6
Shtukar Artem <shtukar95@mail.ru>
2023-06-26 16:18:22 +0300

    0.0.50

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,21 @@
-import { classNames } from "shared/lib/classNames/classNames"
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink"
-import cls from './Navbar.module.scss'
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
-  className?:string
+  className?: string;
 }
 
-
-export const Navbar = ({className}:NavbarProps) => {
+export const Navbar = ({ className }: NavbarProps) => {
   return (
-    <div className={classNames(cls.navbar,{} ,[className])}>
+    <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}></div>
-      <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to='/'>Главная страница</AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>О сайте</AppLink>
+      <AppLink theme={AppLinkTheme.SECONDARY} className={cls.mainLink} to='/'>
+        Главная страница
+      </AppLink>
+      <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>
+        О сайте
+      </AppLink>
     </div>
-  )
-}
+  );
+};
