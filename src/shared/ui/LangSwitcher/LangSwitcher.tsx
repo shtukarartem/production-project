f57013660e31b147ee21356a19b19f7e7b9b63a3
Shtukar Artem <shtukar95@mail.ru>
2023-07-04 16:55:45 +0300

    add stylelint, i18n plugin for eslint

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from '../Button/Button';
-import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
   className?: string;
@@ -13,7 +12,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
     i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
   };
   return (
-      <Button className={classNames(cls.langSwitcher, {}, [className])} theme={ThemeButton.CLEAR} onClick={toggle}>
+      <Button className={classNames('', {}, [className])} theme={ThemeButton.CLEAR} onClick={toggle}>
         {t('Язык')}
       </Button>
   );
