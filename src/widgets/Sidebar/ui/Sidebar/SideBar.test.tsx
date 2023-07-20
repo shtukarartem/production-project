52514b131509dd21c203086715d6913e734f3cdb
Shtukar Artem <shtukar95@mail.ru>
2023-07-20 10:55:11 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/SideBar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/SideBar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithoutTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar';
 
 describe('SideBar', () => {
   test('with only first param', () => {
-    renderWithoutTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('with only first param', () => {
-    renderWithoutTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
